c3fca5ad08a39902595fac9d8d1189a713f92716
Devan Buggay <sbuggay@gmail.com>
2016-02-16 10:46:38 -0600

    Initial move to gitlab

---
+++ src/test/java/com/devan/api/MainApiControllerTest.java
@@ -0,0 +1,53 @@
+package com.devan.api;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dbuggay on 2/16/2016.
+ */
+public class MainApiControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateServer() throws Exception {
+
+    }
+
+    @Test
+    public void testDestroyServer() throws Exception {
+
+    }
+
+    @Test
+    public void testExecuteServer() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAllServers() throws Exception {
+
+    }
+
+    @Test
+    public void testGetServer() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSupportedGames() throws Exception {
+
+    }
+}
